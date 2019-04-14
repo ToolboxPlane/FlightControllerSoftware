aeef017cd4a4548792887be76a3d185140b59721
Paul Nykiel <paul.nykiel@gmail.com>
2019-04-14 19:49:02 +0200

    Started with the main code, updated submodule

--- main.c
+++ main.c
@@ -1,3 +1,17 @@
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wmissing-noreturn"
+
+#include <avr/interrupt.h>
+#include <avr/delay.h>
+
+#include <stdbool.h>
+
 int main(void) {
-    return 0;
+    cli();
+
+    sei();
+    while (true) {
+    }
 }
+
+#pragma clang diagnostic pop
