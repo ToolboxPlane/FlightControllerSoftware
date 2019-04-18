dce2a7402157a77954003c3550ac9c6cd5e62fc7
Paul Nykiel <paul.nykiel@gmail.com>
2019-04-18 20:19:02 +0200

    BNO055 finished

--- Drivers/bno055_uart.h
+++ Drivers/bno055_uart.h
@@ -8,6 +8,34 @@
 #ifndef FLIGHTCONTROLLER_BNO055_UART_H
 #define FLIGHTCONTROLLER_BNO055_UART_H
 
+#include <stdint.h>
+
 void bno055_init(void);
+
+int16_t bno055_acc_x(void);
+int16_t bno055_acc_y(void);
+int16_t bno055_acc_z(void);
+
+int16_t bno055_mag_x(void);
+int16_t bno055_mag_y(void);
+int16_t bno055_mag_z(void);
+
+int16_t bno055_gyr_x(void);
+int16_t bno055_gyr_y(void);
+int16_t bno055_gyr_z(void);
+
+int16_t bno055_eul_x(void);
+int16_t bno055_eul_y(void);
+int16_t bno055_eul_z(void);
+
+int16_t bno055_linear_acc_x(void);
+int16_t bno055_linear_acc_y(void);
+int16_t bno055_linear_acc_z(void);
+
+int16_t bno055_grv_x(void);
+int16_t bno055_grv_y(void);
+int16_t bno055_grv_z(void);
+
+uint8_t bno055_calib_stat(void);
 
 #endif //FLIGHTCONTROLLER_BNO055_UART_H
