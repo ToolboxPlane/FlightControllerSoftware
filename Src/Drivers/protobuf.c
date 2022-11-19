12a1a4e33592e1198cafd403bf41253048f75a17
Paul Nykiel <paul.nykiel@gmail.com>
2022-11-19 18:28:14 +0100

    Added protobuf tests

---
+++ Src/Drivers/protobuf.c
@@ -0,0 +1,10 @@
+/**
+ * @file protobuf_coding.c
+ * @author paul
+ * @date 19.11.22
+ * Description here TODO
+ */
+#include "protobuf.h"
+
+#include <Messages/MessageEncoding.h>
+#include <Messages/MessageDecoding.h>
---
+++ Tests/LowLevel/Drivers/protobuf.cpp
@@ -0,0 +1,6 @@
+/**
+ * @file protobuf.cpp
+ * @author paul
+ * @date 19.11.22
+ * Description here TODO
+ */
