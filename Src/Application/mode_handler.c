dbaf66241c79e7b2aad7e1812aabee2eef2f2c7a
Paul Nykiel <paul.nykiel@gmail.com>
2022-12-21 20:43:13 +0100

    Added mode handler structure

---
+++ Src/Application/mode_handler.c
@@ -0,0 +1,7 @@
+/**
+ * @file mode_handler.c
+ * @author Paul Nykiel
+ * @date 21.12.22
+ * @brief Description here TODO
+ */
+#include "mode_handler.h"
---
+++ Src/Application/mode_handler.h
@@ -0,0 +1,10 @@
+/**
+ * @file mode_handler.h
+ * @author Paul Nykiel
+ * @date 21.12.22
+ * @brief Description here TODO
+ */
+#ifndef FLIGHTCONTROLLER_MODE_HANDLER_H
+#define FLIGHTCONTROLLER_MODE_HANDLER_H
+
+#endif // FLIGHTCONTROLLER_MODE_HANDLER_H
