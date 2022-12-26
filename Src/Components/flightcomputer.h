83c151e7a02621c98b01259ebb789dcac06a4dc0
Paul Nykiel <paul.nykiel@gmail.com>
2022-12-26 20:40:36 +0100

    IMU and Flightcomputer doc (#48)

--- Src/Components/flightcomputer.h
+++ Src/Components/flightcomputer.h
@@ -15,18 +15,40 @@
 #include "remote.h"
 #include "servo_motor.h"
 
+/**
+ * Setpoint as sent by the flightcomputer.
+ */
 typedef struct {
-    uint16_t motor;
-    int16_t pitch, roll;
+    uint16_t motor; ///< The desired motor velocity (in [0, 1000])
+    int16_t pitch; ///< The desired pitch angle (in degree)
+    int16_t roll; ///< The desired roll angle (in degree)
 } flightcomputer_setpoint_t;
 
+/**
+ * Initialize the connection to the flightcomputer by initializing the protobuf module.
+ */
 void flightcomputer_init(void);
 
+/**
+ * Send a message to the flightcomputer by converting the structs to a single protobuf message
+ * and sending the message using the protobuf module.
+ * @param imu_data the IMU data to send
+ * @param remote_data the remote control data to send
+ * @param servo_motor_cmd the servo and motor command to send
+ */
 void flightcomputer_send(const imu_data_t *imu_data, const remote_data_t *remote_data,
                          const servo_motor_cmd_t *servo_motor_cmd);
 
+/**
+ * Checks whether the protobuf module decoded a new message.
+ * @return true if a new message exists, otherwise false
+ */
 bool flightcomputer_setpoint_available(void);
 
+/**
+ * Gets the latest message from the protobuf module and converts it to a flightcomputer_setpoint_t
+ * @return the converted setpoint
+ */
 flightcomputer_setpoint_t flightcomputer_get_setpoint(void);
 
 #endif // FLIGHTCONTROLLER_FLIGHTCOMPUTER_H
--- Src/Components/imu.h
+++ Src/Components/imu.h
@@ -12,10 +12,30 @@
 #include <stdbool.h>
 #include <stdint.h>
 
+/**
+ * Set of measurements from
+ */
 typedef struct {
+    /**
+     * Orientation in euler angles (degree) multiplied by 16
+     */
     int16_t heading_mul_16, pitch_mul_16, roll_mul_16;
+
+    /**
+     * Angular rates in euler angles (degree/second) multiplied by 16
+     */
     int16_t d_heading_mul_16, d_pitch_mul_16, d_roll_mul_16;
+
+    /**
+     * Acceleration in all three axis (m/s^2) multiplied by 100
+     */
     int16_t acc_x_mul_100, acc_y_mul_100, acc_z_mul_100;
+
+    /**
+     * Signals if the received data is fully usable, i.e.:
+     *  * the system status is ok
+     *  * all sensors are sufficiently calibrated
+     */
     bool imu_ok;
 } imu_data_t;
 
@@ -25,9 +45,38 @@ typedef enum {
     IMU_ERROR_STATUS = 3,
 } imu_error_t;
 
+/**
+ * Initialize the IMU component.
+ * First the bno055 module is initialized and then the following operations are performed:
+ *  * Set the chip to config mode
+ *  * Run the self test (and check the result)
+ *  * Configure the units
+ *  * Remap the axis and corresponding signs
+ *  * Set the sensor NDOF-FMC-OFF (9 degrees of freedom - fast magnetic calibration off)
+ */
 void imu_init(void);
+
+/**
+ * Start sampling of the sensor.
+ *
+ * This reads all data into an internal imu_data_t struct, if an error occurs sampling is aborted (and no new data
+ * is reported).
+ *
+ * @warning Currently only one datum is read per call to this function, i.e. multiple calls are necessary to read
+ * all data (see #41).
+ */
 void imu_start_sampling(void);
+
+/**
+ * Get the last, fully received, set of measurements.
+ * @return a struct containing all measurement data.
+ */
 imu_data_t imu_get_latest_data(void);
+
+/**
+ * Checks whether a new set of measurements was collected since the last call to get_latest_data().
+ * @return true if new data is available.
+ */
 bool imu_data_available(void);
 
 #endif // FLIGHTCONTROLLER_IMU_H
