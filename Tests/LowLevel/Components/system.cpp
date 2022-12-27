3d6b3addf702d0745bda3ac55ebfc7e5e64e4be3
Paul Nykiel <paul.nykiel@gmail.com>
2022-12-27 09:52:43 +0100

    #45: Better runtime tests

--- Tests/LowLevel/Components/system.cpp
+++ Tests/LowLevel/Components/system.cpp
@@ -59,7 +59,7 @@ TEST(TEST_NAME, post_init) {
     EXPECT_TRUE(wdtHandle.functionGotCalled<wdt_enable>(WDTO_30MS));
 }
 
-TEST(TEST_NAME, timer_runtime_normal) {
+TEST(TEST_NAME, timer_runtime_0) {
     auto interruptHandle = mock::interrupt.getHandle();
     auto wdtHandle = mock::wdt.getHandle();
     auto timerHandle = mock::timer8bit.getHandle();
@@ -80,7 +80,7 @@ TEST(TEST_NAME, timer_runtime_normal) {
     EXPECT_TRUE(timerHandle.functionGotCalled<timer_8bit_get_count>());
 }
 
-TEST(TEST_NAME, timer_runtime_exception) {
+TEST(TEST_NAME, timer_runtime_max) {
     auto interruptHandle = mock::interrupt.getHandle();
     auto wdtHandle = mock::wdt.getHandle();
     auto timerHandle = mock::timer8bit.getHandle();
@@ -92,6 +92,52 @@ TEST(TEST_NAME, timer_runtime_exception) {
                 internalCallback = timerCallback;
             });
     timerHandle.overrideFunc<timer_8bit_get_count>([]() { return 255; });
+
+    system_pre_init(timer_callback);
+    system_post_init();
+
+    timer_got_called = false;
+    internalCallback();
+    EXPECT_TRUE(timer_got_called);
+    EXPECT_TRUE(timerHandle.functionGotCalled<timer_8bit_get_count>());
+    EXPECT_TRUE(errorHandlerHandle.functionGotCalled<error_handler_handle_warning>(SYSTEM, SYSTEM_ERROR_TIMER_RUNTIME));
+}
+
+TEST(TEST_NAME, timer_runtime_under_limit) {
+    auto interruptHandle = mock::interrupt.getHandle();
+    auto wdtHandle = mock::wdt.getHandle();
+    auto timerHandle = mock::timer8bit.getHandle();
+
+    timer_8bit_callback_t internalCallback = nullptr;
+    timerHandle.overrideFunc<timer_8bit_init>(
+            [&internalCallback](timer_8bit_clock_option_t /*clock_option*/, system_timer_callback_t timerCallback) {
+              internalCallback = timerCallback;
+            });
+    // 12/16,384*255=186.767578125
+    timerHandle.overrideFunc<timer_8bit_get_count>([]() { return 186; });
+
+    system_pre_init(timer_callback);
+    system_post_init();
+
+    timer_got_called = false;
+    internalCallback();
+    EXPECT_TRUE(timer_got_called);
+    EXPECT_TRUE(timerHandle.functionGotCalled<timer_8bit_get_count>());
+}
+
+TEST(TEST_NAME, timer_runtime_over_limit) {
+    auto interruptHandle = mock::interrupt.getHandle();
+    auto wdtHandle = mock::wdt.getHandle();
+    auto timerHandle = mock::timer8bit.getHandle();
+    auto errorHandlerHandle = mock::error_handler.getHandle();
+
+    timer_8bit_callback_t internalCallback = nullptr;
+    timerHandle.overrideFunc<timer_8bit_init>(
+            [&internalCallback](timer_8bit_clock_option_t /*clock_option*/, system_timer_callback_t timerCallback) {
+              internalCallback = timerCallback;
+            });
+    // 12/16,384*255=186.767578125
+    timerHandle.overrideFunc<timer_8bit_get_count>([]() { return 187; });
 
     system_pre_init(timer_callback);
     system_post_init();
