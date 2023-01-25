df333ca8794e189a8dda31c3511348f6728efc2b
Paul Nykiel <paul.nykiel@gmail.com>
2023-01-25 20:02:29 +0100

    Started with SBus Requirements

--- Src/Drivers/sbus.h
+++ Src/Drivers/sbus.h
@@ -27,18 +27,55 @@ typedef struct {
 } sbus_data_t;
 
 /**
- * Initializes the SBUS module by initializing all internal data and the UART connection.
+ * @Initialize the SBUS module.
+ *
+ * The initialization consists of the following tasks:
+ *  * Initialize the internal ring buffer
+ *  * Initialize the uart to the following parameters (::uart_init):
+ *      * ID: 2
+ *      * Baud Rate: 100 000
+ *      * Parity: Even
+ *      * Stop Bits: 2
+ *      * Callback: pointer to the internal uart callback function
+ *  * Initialize all internal state for decoding
+ *
+ * The internal uart callback function performs the following tasks:
+ *  * Add the received byte to the internal ring-buffer
+ *
  */
 void sbus_init(void);
 
 /**
- * Decodes all data received since the last call and returns whether the data contains a full new packet.
+ * @brief Decodes all data received since the last call and returns whether the data contains a full new packet.
+ *
+ * This function iterates over all in the ring buffer and feeds the bytes, in the order they were received,
+ * into the following state machine:
+ *
+ * \dot
+ * digraph example {
+ *      node [shape=record, fontname=Helvetica, fontsize=10];
+ *      INIT;
+ *      START_RECEIVED;
+ *      IN_DATA;
+ *      AT_FLAGS;
+ *      AT_END;
+ *
+ *      INIT -> START_RECEIVED [
+ *          label = "data=0x0F";
+ *      ]
+ * }
+ * \enddot
+ *
  * @return true if a new packet was received, otherwise false.
  */
 bool sbus_data_available(void);
 
 /**
- * Get the last packet that was received.
+ * @brief Get the last packet that was received.
+ *
+ * Returns the last fully received package, if no package was received both the failsafe and the frame-lost flag
+ * shall be true.
+ *
  * @return a copy of the last packet.
  */
 sbus_data_t sbus_get_latest_data(void);
